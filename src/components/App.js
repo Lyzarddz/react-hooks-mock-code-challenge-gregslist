989a439d64c41cc457f0f291dda11a007b06c24b
Alyssa Falcione <lyzarddz@gmail.com>
2022-04-07 11:39:12 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [search, setSearch] = useState("")
+  const[listings, setListings] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((resp) => resp.json())
+    .then((data) => setListings(data)
+    )
+  },[])
+
+  const listingCards = listings
+  .filter((listing) =>  
+  listing.description.toLowerCase().includes(search.toLowerCase()))
+
+
+  function handleSearch(newSearch){
+    setSearch(newSearch)
+  }
+  
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearch} listingCards={listingCards}/>
+      <ListingsContainer search={search} listings={listingCards} setListings={setListings}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,30 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings, setListings}) {
+ 
+
+  function handleListingDelete(id) {
+    const updatedListings = listings.filter(
+      (listing) => 
+      listing.id !==id
+    )
+    setListings(updatedListings)
+  }
+
+
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+
+      <ul className="cards"> 
+        {listings.map((l) => {
+          return (<ListingCard
+          key={l.id}
+          listings={l}
+          onDeleteListing={handleListingDelete}
+            />)
+        })
+      }
       </ul>
     </main>
   );
